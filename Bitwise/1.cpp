44ac660eed3bb4a55cf72f4e7fb16c7ab0282233
LuckyChaudhari05 <166679369+LuckyChaudhari05@users.noreply.github.com>
2025-06-13 13:26:07 +0530

    Modifed 1.cpp

--- Bitwise/1.cpp
+++ Bitwise/1.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 
 int main() {
-  int a = 2;
+  int a = 3;
+  int b = 6;
+
   // for(int i = 0 ; i <= 9 ; i++) {
   //   cout << ~i << endl;
   // } 
+
+  cout << " a&b :  " <<  (a & b) <<endl;
+  cout << " a|b :  " <<  (a | b) <<endl;
+  cout << "  ~a :  " <<  ~a << endl;
+  cout << " a^b :  " <<  (a ^ b) <<endl;
+  cout << " a<b :  " <<  (a < b) <<endl;
+  cout << " a>b :  " <<  (a > b) <<endl;
+
   return 0;
 }
